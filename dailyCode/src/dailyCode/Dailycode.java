44dfe14ef4eb647114a207cd092a6594d96dae54
sai3690 <saisamarpan2012@gmail.com>
2022-04-24 10:04:38 +0530

    Reverse a String

--- dailyCode/src/dailyCode/Dailycode.java
+++ dailyCode/src/dailyCode/Dailycode.java
@@ -1,11 +1,29 @@
 package dailyCode;
 
+import java.util.Scanner;
+
 public class Dailycode {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world");
-
+		System.out.println("Print a number");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int check = n;
+		int temp;	
+		int rev=0;
+		while(n>0)
+		{
+		temp = n%10;
+		rev = rev*10+temp;
+		n=n/10;
+		}
+		if(rev==check) {
+			System.out.println("Entered number is palindrome");
+		}
+		else {
+			System.out.println("entred number is not palindrome");
+		}
 	}
 
 }
